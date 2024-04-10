9465cb8c9c1632ea8dd9adeabdd8c722fa066f33
Manoj Kumar <you@example.com>
2024-04-10 17:21:52 +0530

    multiple component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-
+const Hello=()=>{
+  return(
+    <div>
+      <p>hello my namr </p>
+    </div>
+  )
+}
 const App=()=>{
 const now = new Date;
 const a=20;
@@ -6,9 +12,11 @@ const b=25;
   return(
     <div>
       <p>hello world</p>
-      <p>numner {a} plus {b} ={a+b}</p>
-      <h1>finally I wrote my first code in react I am a {now.toString()}Stock developer</h1>
+      <Hello/>
+      <p>numner {a} plus {b} ={a+b}</p><Hello/>
+      <h1>finally I wrote my first code in react I am a {now.toString()}Stock developer</h1><Hello/>
     </div>
+  
   )
 }
 export default App;
