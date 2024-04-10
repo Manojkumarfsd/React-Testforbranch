7f2fba85b73176d9db1b892b6ba55cf63fe4a402
Manoj Kumar <you@example.com>
2024-04-10 22:26:12 +0530

    working with array list

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
-import './App.css'
-let Hello= (props)=>{
-  
-  return (
-    <div>
-      <h1>Hi how are you by {props.me}</h1>
-    </div>
-  )
-}
-let App=()=>{
-  let name="manoj" 
-  let age="25"
-  return(
-<div>
-  <Hello me="full stack developer"/>
-  <p> {name} is a full stack developer, his age is new {age} </p>
-</div>
+import React from 'react'
 
+const App=(props)=>{
+const {notes}=props;
+  return(
+    <div>
+      <ul>
+        <li>{notes[0].content}</li>
+        <li>{notes[1].content}</li>
+        <li>{notes[2].content}</li>
+      </ul>
+
+    </div>
   )
 }
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const notes =[
+    {
+        id:1,
+        content:"working with array",
+        important:true
+    },
+    {
+        id:2,
+        content:"usage of keys",
+        important:true
+    },
+    {
+        id:3,
+        content:"conditional rendering",
+        important:false
+    }
+]
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes}/>);
