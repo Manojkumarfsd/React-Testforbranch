1bf38d66cd9bff9e003f581ea8fbd03d555e250a
Manoj Kumar <you@example.com>
2024-04-10 16:34:05 +0530

    dynamic code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 
 const App=()=>{
-
+const now = new Date;
+const a=20;
+const b=25;
   return(
     <div>
       <p>hello world</p>
-      <h1>finally I wrote my first code in react</h1>
+      <p>numner {a} plus {b} ={a+b}</p>
+      <h1>finally I wrote my first code in react I am a {now.toString()}Stock developer</h1>
     </div>
   )
 }
