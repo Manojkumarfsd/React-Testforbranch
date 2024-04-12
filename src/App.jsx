79b7e409f162ae13b0197513ebdb67c6809ab200
Manoj Kumar <you@example.com>
2024-04-13 00:14:26 +0530

    React with cdnj

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./App.css"
 function App(){
   return(
-    <div>
-      <h1>header</h1>
-      <p>this paragraph is green</p>
+    <div className='mt-4 p-4'>
+      <h1 >header</h1>
+      <p className="red">this paragraph is green</p>
     </div>
   )
 }
