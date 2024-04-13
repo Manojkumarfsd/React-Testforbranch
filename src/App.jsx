6d96adfdf1df9af180dc6ad753f7de7cdce67944
Manoj Kumar <you@example.com>
2024-04-13 13:48:17 +0530

    React with handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 function App(){
-  const [counter,setcounter]=useState(0);
-  setTimeout(()=>{
-    setcounter(counter+1)
-  },1000)
+  const[counter,setcounter]=useState(0);
+  function handler(){
+    setTimeout(() => {
+      setcounter(counter+1);
+    },1000 );
+  }
+
   return(
     <div>
+      
+      <button onClick={handler}>click me</button>
       {counter}
     </div>
   )
