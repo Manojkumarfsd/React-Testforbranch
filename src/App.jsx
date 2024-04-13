a144d6408675c3efe8b06a19f124511bcedae36a
Manoj Kumar <you@example.com>
2024-04-13 14:04:19 +0530

    multiple handler in usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
 import React, { useState } from 'react'
 function App(){
   const[counter,setcounter]=useState(0);
-  function handler(){
-    setTimeout(() => {
+  function handlerplus(){
       setcounter(counter+1);
-    },1000 );
-  }
+    };
+    function handlerzero(){
+      setcounter(0)
+    };
+    function handlerminus(){
+      setcounter(counter-1);
+
+    }
+
+  
 
   return(
+    <div className='mt-4 p-4'>
     <div>
       
-      <button onClick={handler}>click me</button>
-      {counter}
+      <button onClick={handlerplus}>plus</button>
+      <button onClick={handlerzero}>zero</button>
+      <button onClick={handlerminus}>minus</button>
+      
+    </div>
+    <div className ='mt-4 p-4'>
+    {counter}
+    </div>
+
     </div>
   )
 }
+
 export default App;
