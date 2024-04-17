018296429ec7ed19a513410b28d3d3949ed833ff
Manoj Kumar <you@example.com>
2024-04-17 16:30:16 +0530

    Basic form with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,54 @@
-import React, { createContext, useState } from 'react'
-import ChildComponent from './components/ChildComponent';
+import React, { useState } from 'react'
+import Note from './components/Note'
 
-const MessageContext= createContext();
-function App() {
-  const [message,setmessage] = useState('hello from App'); 
+
+
+function App(props) {
+  const [notes,setNotes]=useState(props.notes)
+  const [newNote, setNewnote]= useState(" ")
+
+  const addNote=(event)=>{
+    event.preventDefault();
+    
+    const noteobject ={
+      id:notes.length+1,
+      content: newNote,
+      important:  Math.random()< 0.5 }
+
+      setNotes(notes.concat(noteobject));
+      setNewnote(" ");
+    
+  }
+
+  const handleNoteChange =(event)=>{
+    setNewnote(event.target.value)
+
+
+  }
+
   return (
     <div>
-      <h1>Parent component</h1>
-      <MessageContext.Provider value={message}>
-      <ChildComponent/> 
-      </MessageContext.Provider> 
+      <h1>Notes</h1>
+      <ul>
+        {
+          notes.map(note=>
+          <Note key={note.id}note={note}/> 
 
+          )}
+        
+      </ul>
+      <form onSubmit={addNote}>
+        <input  
+        value={newNote}
+        placeholder='a new note'
+        onChange={handleNoteChange}
+        />
+        <button type='submit'>save</button>
+      </form>
+
+   
     </div>
   )
 }
 
-export {App as default, MessageContext} ;
+export default App;
